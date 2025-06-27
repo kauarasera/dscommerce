5f10602fccc832092a26be1728a6f58daac67122
Kauã Rasera <kauarasera@gmail.com>
2025-06-27 08:15:54 +0100

    DSCommerce created

---
+++ src/test/java/com/kauarasera/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauarasera.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

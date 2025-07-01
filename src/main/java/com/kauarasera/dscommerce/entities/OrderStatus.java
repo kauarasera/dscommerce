1b6cf3b2b8c3651e190f6d1e5ecee72dd0584078
Kauã Rasera <kauarasera@gmail.com>
2025-07-01 08:30:06 +0100

    Database seeding

--- src/main/java/com/kauarasera/dscommerce/entities/OrderStatus.java
+++ src/main/java/com/kauarasera/dscommerce/entities/OrderStatus.java
@@ -1,5 +1,7 @@
 package com.kauarasera.dscommerce.entities;
 
 public enum OrderStatus {
+    ///
+    /// 0         1       2         3          4
     WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
 }
